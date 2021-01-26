7af8aa17848cef6c58a652fdcc0dcda075276354
tien dat <tiendat@hsvina.com>
2021-01-26 21:53:20 +0700

    Init all Source

---
+++ src/Footer/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './style.scss';
+
+Footer.propTypes = {
+
+};
+
+function Footer(props) {
+    return (
+        <div>
+            <div className="footer">
+                Footer
+            </div>
+        </div>
+    );
+}
+
+export default Footer;
---
+++ src/Header/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './style.scss';
+
+Header.propTypes = {
+
+};
+
+function Header(props) {
+    return (
+        <div>
+            <div className="header">
+                Header
+            </div>
+        </div>
+    );
+}
+
+export default Header;
